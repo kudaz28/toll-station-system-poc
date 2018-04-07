6cfc70c3c3cab0ec20cc28f735b436569c0f4f35
William Zhou <williamkzhou@hotmail.com>
2018-04-07 15:01:54 +0100

    Initial commit of Application scaffolding .

---
+++ toll-station-system/src/main/java/org/williamkzhou/springboot/com/tollstationsystem/TollStationSystemApplication.java
@@ -0,0 +1,12 @@
+package org.williamkzhou.springboot.com.tollstationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollStationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollStationSystemApplication.class, args);
+	}
+}
